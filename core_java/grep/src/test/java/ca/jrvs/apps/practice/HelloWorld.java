e135faf6391e511087a8edb7b8fbf506804101a2
Paul Guevarra <paulwilsonguevarra@gmail.com>
2021-11-04 17:48:30 +0000

    Edited JavaGrep

---
+++ core_java/grep/src/test/java/ca/jrvs/apps/practice/HelloWorld.java
@@ -0,0 +1,10 @@
+package ca.jrvs.apps.practice;
+import java.util.*;
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        //ArrayList<Integer> i = new ArrayList<Integer>();
+
+    }
+
+}
